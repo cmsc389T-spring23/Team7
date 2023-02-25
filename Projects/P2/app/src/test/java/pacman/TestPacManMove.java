1f9eed151d95cd5466b4b2b5260ac62d5ead443a
RitvikY <ritviky@terpmail.umd.edu>
2023-02-25 12:19:16 -0500

    adding pacman move tests

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -4,8 +4,21 @@ import junit.framework.*;
 import java.awt.Color;
 
 public class TestPacManMove extends TestCase {
-
   public void testPacManMove() throws FileNotFoundException {
-    return;
+    NoFrame frame = null;
+			try {
+				frame = new NoFrame();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println(e);
+			}
+			Map map = frame.getMap();
+
+			PacMan pac = frame.addPacMan(new Location(1, 1));
+
+			assertEquals(true, pac.move());
+			assertEquals(true, pac.move());
+			assertEquals(true, pac.move());
+
   }
 }
