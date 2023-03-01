909714554f58a918adfabb6cbb16448bf3d644ff
Rishabh Karanth <68529712+rishkar@users.noreply.github.com>
2023-03-01 17:28:23 -0500

    Revert "pacman consume functionality to FTR-pacman"

--- Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
@@ -2,20 +2,10 @@ package pacman;
 import java.io.*;
 import junit.framework.*;
 import java.awt.Color;
-import NoFrame;
 
 public class TestPacManConsume extends TestCase {
 
   public void testPacManConsume() throws FileNotFoundException {
-    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-
-    //Creating Players
-    PacMan pacman = frame.addPacMan(new Location(1, 1)); 
-    CookieComponent cookie = new CookieComponent(1, 1, 0);
-
-    frame.getMap().add("tok_x1_y1", new Location(1,1), cookie, Map.Type.COOKIE);
-
-    assert pacman.consume().equals(cookie);
-    
+    return;
   }
 }
