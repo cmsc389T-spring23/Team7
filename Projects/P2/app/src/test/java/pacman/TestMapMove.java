0e53184d232e0963573cbba5d2416c0fa5e667f2
Benjamin Jean <benjamin2jean@yahoo.com>
2023-03-01 22:30:27 -0500

    adding tests for map-move

--- Projects/P2/app/src/test/java/pacman/TestMapMove.java
+++ Projects/P2/app/src/test/java/pacman/TestMapMove.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return;
+  NoFrame frame= new NoFrame();
+  PacMan pacman = frame.addPacMan(new Location(3, 5));
+
+  assertTrue(map.move("pacman", new Location (3,6), Map.Type.PACMAN));
   }
 }
