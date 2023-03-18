e7c474841b7ad9e8eb9327f5ad86873ac2d749d3
Abhinav Yedla <abhinavyedla02@gmail.com>
2023-03-18 16:20:10 -0400

    Add main.yml for GitHub Actions setup

--- Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
@@ -6,6 +6,27 @@ import java.awt.Color;
 public class TestPacManInRange extends TestCase {
 
   public void testPacManInRange() throws FileNotFoundException {
-    return;
+    Map gameMap = new Map();
+
+    Location pacManLoc = new Location(9, 12);
+    Location ghostLoc = new Location(9, 11);
+    PacMan pacMan = new PacMan(pacManLoc, gameMap);
+    Ghost ghost = new Ghost(ghostLoc, gameMap, "clyde");
+
+    gameMap.move("pacman", pacManLoc, Map.Type.PACMAN);
+    gameMap.move("clyde", ghostLoc, Map.Type.GHOST);
+
+    // Test if PacMan is in range of the ghost
+    boolean result = ghost.is_pacman_in_range();
+    assertTrue(result);
+
+    //move
+    Location newPacManLoc = new Location(10, 12);
+    gameMap.move("pacman", newPacManLoc, Map.Type.PACMAN);
+    pacMan.myLoc = newPacManLoc;
+
+    // Test if PacMan is still in range of the ghost
+    result = ghost.is_pacman_in_range();
+    assertFalse(result);
   }
 }
